0684a6c4aa739332101168782f5c1de2186b4944
Letícia Neiva <leh.haitani@gmail.com>
2023-05-16 11:26:31 -0300

    settings

--- polls/models.py
+++ polls/models.py
@@ -1,19 +1,12 @@
 from django.db import models
 
-class Usuario(models.Model):
-    nome = models.CharField(max_length=50)
-    nickname = models.CharField(max_length=100, unique=True)
-    avatar = models.CharField(max_length=225, null=True)
 
-class Questionario(models.Model):
-    nome = models.CharField(max_length=45)
-    descricao = models.CharField(max_length=500, null=True)
-    data_criacao = models.DateTimeField(auto_now_add=True)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
 
-class Hashtag(models.Model):
-    tag = models.CharField(max_length=100)
 
-class HashtagHasQuestionario(models.Model):
-    hashtag = models.ForeignKey(Hashtag, on_delete=models.CASCADE)
-    questionario = models.ForeignKey(Questionario, on_delete=models.CASCADE)
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
