035e3c2e05e2a0975dc5e40d5347de5d6a1663c0
Letícia Neiva <leh.haitani@gmail.com>
2023-06-06 11:32:48 -0300

    questions and pages

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
+
 def index(request):
-    return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse(f"Essa é a pergunta de número {question_id}.")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Resultados da pergunta de número {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Você vai votar na pergunta de número {question_id}.")
