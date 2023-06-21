315bb42506e01208cf7a3e365fb7a4fbb7910561
Letícia Neiva <leh.haitani@gmail.com>
2023-06-21 09:33:02 -0300

    index

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return HttpResponse(f"Essa é a pergunta de número {question_id}.")
