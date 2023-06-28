8fb8e326d30c28b765746c8ca1b79e4118d9a6d4
Letícia Neiva <leh.haitani@gmail.com>
2023-06-28 09:29:33 -0300

    html

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,4 @@
-from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Question
 
@@ -10,12 +9,14 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse(f"Essa é a pergunta de número {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
-    return HttpResponse(f"Resultados da pergunta de número {question_id}.")
+    response = f"Resultados da pergunta de número {question_id}."
+    return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Você vai votar na pergunta de número {question_id}.")
+    return HttpResponse (f"Você vai votar na pergunta de número {question_id}.")
